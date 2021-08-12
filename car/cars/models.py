708d415e0bfb7305bda7cf128eb7afe294baa805
Ammar sahyoun <ammarzgr@hotmail.com>
2021-08-12 19:58:02 +0200

    Add models and create db

--- car/cars/models.py
+++ car/cars/models.py
@@ -1,6 +1,18 @@
+from datetime import date
 from django.db import models
 
-class car(models.Model):
-    type = models.charField(max_length=20)
+
+class Owner(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Owner name: {self.name}"
+
+class Car(models.Model):
+    type = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
-    year = models.DateField()
+    year = models.DateField(default=date(year=1950, month=1, day=1))
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.type}, {self.model}, made on {self.year}"
