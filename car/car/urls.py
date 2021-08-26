f86214434225de16597ffc68f672fb5161c7c1e7
Ammar sahyoun <ammarzgr@hotmail.com>
2021-08-26 20:53:57 +0200

    create owner's car_under_test

--- car/car/urls.py
+++ car/car/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from website.views import welcome , about #date
-
+from website.views import welcome
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name='welcome'),
-    #path('date', date),
-    path('about',about),
     path('cars/', include('cars.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
+
+
+
+
--- car/cars/urls.py
+++ car/cars/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:id>', views.detail, name="detail"),
+    path('<int:id>', views.detail, name="detail_name"),
     path('owners', views.owner_list, name="owners"),
     path('new', views.new, name="new"),
     path('about',views.about, name='about'),
+    #path('owner/<int:id>', views.owner_cars, name='o_cars'),
 
 ]
